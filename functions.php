3bde972fc9e600e3f02dfb51541633a07a41ec7b
Jaap Wiering <github.com@fanagalo.nl>
2025-07-07 12:30:40 +0200

    serif font Petrona, font Bricolage as default

--- functions.php
+++ functions.php
@@ -3,8 +3,7 @@
  * Enqueue style.css to directly add custom CSS code
  */
 
-function fngllw_enqueue_styles()
-{
+function fngllw_enqueue_styles() {
     wp_enqueue_style('additional-style', get_stylesheet_uri(), [], wp_get_theme()->get('Version'));
 }
 add_action('wp_enqueue_scripts', 'fngllw_enqueue_styles');
@@ -13,23 +12,17 @@ add_action('wp_enqueue_scripts', 'fngllw_enqueue_styles');
 /**
  * Enqueue theme styles and fonts to make fonts available globally on the frontend
  */
-function fngllw_enqueue_fonts() {
-    wp_enqueue_style(
-            'mytheme-local-fonts',
-            get_template_directory_uri() . '/assets/css/fonts.css',
-            array(),
-            wp_get_theme()->get( 'Version' )
-        );
-    }
-add_action( 'wp_enqueue_scripts', 'fngllw_enqueue_fonts' );
+function fngllw_enqueue_local_fonts() {
+    wp_enqueue_style('local-fonts', get_template_directory_uri() . '/assets/css/fonts.css', array(), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'fngllw_enqueue_local_fonts' );
 
 
 /**
  * Dark mode enqueue style and script
  */
 
-function fngllw_enqueue_darkmode()
-{
+function fngllw_enqueue_darkmode() {
     wp_enqueue_style('darkmode-style', get_template_directory_uri() . '/assets/css/darkmode.css', array(), null);
     wp_enqueue_script('darkmode-toggle', get_template_directory_uri() . '/assets/js/darkmode-toggle.js', array(), null, true);
 }
